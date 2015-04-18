99ce0011a5c9e0608f4a588fabac1bb50f749d9f
crazymeeshu <crazymeeshu@gmail.com>
2015-04-18 02:44:18 -0700

    Updated photo edit page

--- photoedit/www/js/controllers.js
+++ photoedit/www/js/controllers.js
@@ -50,6 +50,118 @@ angular.module('starter.controllers', ['ionic'])
   };
 })
 
+.controller('PhotoEditCtrl', function($scope){
+	$scope.brightnessValue = 0;
+	$scope.noiseValue = 0;
+	$scope.pixelate = 0;
+	$scope.enableBrightness = false;
+	$scope.disableBrightness = false;
+	
+	angular.element(document).ready(function () {
+		var canvas = new fabric.Canvas('c');
+		var f = fabric.Image.filters;
+		var grayFilter = new fabric.Image.filters.Grayscale()
+		var invertFilter = new fabric.Image.filters.Invert()
+		var sepiaFilter = new fabric.Image.filters.Sepia()
+		var brightFilter = new fabric.Image.filters.Brightness({brightness: $scope.brightnessValue})
+		var noiseFilter = new fabric.Image.filters.Noise({noise: $scope.noiseValue})
+		var pixelateFilter = new fabric.Image.filters.Pixelate({blocksize: $scope.pixelateValue})
+		
+		var img = "../tiger.jpg"
+		fabric.Image.fromURL(img, function(outImg) {
+			canvas.add(outImg);
+			canvas.centerObject(outImg);
+			canvas.item(0).lockMovementY = true;
+			canvas.item(0).lockMovementX = true;
+			canvas.item(0).lockScalingX = true;
+			canvas.item(0).lockScalingY = true;
+			canvas.item(0).lockRotation = true;
+			canvas.item(0).hasControls = false;
+			canvas.item(0).hasBorders = false;
+			canvas.renderAll();
+			canvas.setActiveObject(outImg);
+		});
+		
+		$scope.isBright = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(brightFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notBright = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, brightFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.isGray = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(grayFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notGray = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, grayFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.isInvert = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(invertFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notInvert = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, invertFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.isSepia = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(sepiaFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notSepia = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, sepiaFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.isNoise = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(noiseFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notNoise = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, noiseFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.isPixelate = function (){
+			var obj = canvas.getActiveObject();
+			obj.filters.push(pixelateFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		$scope.notPixelate = function (){
+			var obj = canvas.getActiveObject();
+			fabric.util.removeFromArray(obj.filters, pixelateFilter);
+			obj.applyFilters(canvas.renderAll.bind(canvas));
+		};
+		
+		/*$scope.changeBright = function(){
+			console.log($scope.enableBrightness)
+			$scope.enableBrightness = !$scope.enableBrightness;
+			console.log($scope.enableBrightness)
+		};*/
+	})
+})
+
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
